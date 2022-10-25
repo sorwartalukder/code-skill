5299ee6e2100c6f7c1ce8af8b6d53531ca480bf8
sorwartalukder <mdsorwar4039@gmail.com>
2022-10-26 00:48:48 +0600

    add not found page

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const NotFoundPage = () => {
     return (
-        <div>
-            not found
+        <div className='container  w-50 text-center my-5  text-danger shadow-lg p-3  bg-body rounded ' >
+            <h1 style={{ fontSize: '5em' }}>404</h1>
+            <p className='m-0'>You didn't break the internet, but we can't find what you are looking for.</p>
+            <p>Please enter right URL</p>
         </div>
     );
 };
