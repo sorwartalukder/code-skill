44809cbcb47a71850d229a956ce30caa50d311d8
sorwartalukder <mdsorwar4039@gmail.com>
2022-10-25 23:31:58 +0600

    add favicon add title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
+    <RouterProvider router={routes}>
 
-    </div>
+    </RouterProvider>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-dark text-white mt-5 py-5'>
+            <div className='container d-flex justify-content-between '>
+                <h1>Code Skill</h1>
+                <address>
+                    <h3>Address</h3>
+                    <p className='m-0'><small>House: 01, Road: 12, Uttara</small></p>
+                    <p><small>Dhaka, Banglades</small></p>
+                </address>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
