1d4ae0b7900dcb4f27ca0eeb365c0e0c9fe93baa
sorwartalukder <mdsorwar4039@gmail.com>
2022-10-26 00:26:23 +0600

    route setup

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../../components/Blog/Blog";
+import Courses from "../../components/Courses/Courses";
 import Home from "../../components/Home/Home";
+import NotFoundPage from "../../components/NotFoundPage/NotFoundPage";
 import Main from "../../layout/Main";
 
 
@@ -8,10 +11,19 @@ export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <NotFoundPage />,
         children: [
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
             }
         ]
     }
