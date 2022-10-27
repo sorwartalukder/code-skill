b66add4dcf327d1f826a17903214ef645c309082
sorwartalukder <mdsorwar4039@gmail.com>
2022-10-27 11:38:05 +0600

    comfilet

--- src/degine/degine.js
+++
@@ -1,5 +0,0 @@
-// https://themeforest.net/item/edison-online-education-template-for-figma/screenshots/29974395?index=1
-
-// https://preview.themeforest.net/item/coursehub-education-hubspot-theme/full_screen_preview/32549009?_ga=2.192444522.1393062872.1666703566-1177760069.1658420376
-
-// https://preview.themeforest.net/item/edison-online-education-elementor-template-kit/full_screen_preview/30139973?_ga=2.196777196.1393062872.1666703566-1177760069.1658420376
